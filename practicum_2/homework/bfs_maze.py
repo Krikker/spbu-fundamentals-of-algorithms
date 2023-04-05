1ee73e67554ebdba1a55ea9bdd70d5042b3055e3
Krikker <andrey.leonov.2004@mail.ru>
2023-04-05 19:14:06 +0300

    practicum 2 complete

--- practicum_2/homework/bfs_maze.py
+++ practicum_2/homework/bfs_maze.py
@@ -1,5 +1,7 @@
 from time import perf_counter
-
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
 
 class Maze:
     def __init__(self, list_view: list[list[str]]) -> None:
@@ -9,6 +11,10 @@ class Maze:
             if sym == "O":
                 self.start_j = j
 
+        self.start = None
+        self.finish = None
+        self.G = None
+
     @classmethod
     def from_file(cls, filename):
         list_view = []
@@ -17,6 +23,32 @@ class Maze:
                 list_view.append(list(l.strip()))
         obj = cls(list_view)
         return obj
+
+    def get_nx_graph(self):
+        if self.G:
+            return self.G
+        self.G = nx.Graph()
+
+        for i in range(len(self.list_view)):
+            for j in range(len(self.list_view[i])):
+                if self.list_view[i][j] != '#':
+                    if self.list_view[i][j] == 'O':
+                        self.start = (i, j)
+                    if self.list_view[i][j] == 'X':
+                        self.finish = (i, j)
+                    if i + 1 < len(self.list_view) and self.list_view[i + 1][j] != '#':
+                        self.G.add_edge((i, j), (i + 1, j))
+                    if j + 1 < len(self.list_view[i]) and self.list_view[i][j + 1] != '#':
+                        self.G.add_edge((i, j), (i, j + 1))
+        return self.G
+
+    def draw_graph(self):
+        self.G = self.get_nx_graph()
+        pos = dict()
+        for i, j in set(self.G):
+            pos[(i, j)] = np.array([j, -i])
+        nx.draw(self.G, pos, with_labels=True)
+        plt.show()
 
     def print(self, path="") -> None:
         # Find the path coordinates
@@ -38,13 +70,29 @@ class Maze:
 
 def solve(maze: Maze) -> None:
     path = ""  # solution as a string made of "L", "R", "U", "D"
+    G = maze.get_nx_graph()
+    node_reverse_paths = dict()
+    for node1, node2 in nx.bfs_edges(G, source=maze.start):
+        node_reverse_paths[node2] = node1
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    x = maze.finish
+    while x != maze.start:
+        path += _get_step(node_reverse_paths[x], x)
+        x = node_reverse_paths[x]
+    path = path[:: -1]
 
     print(f"Found: {path}")
     maze.print(path)
+
+def _get_step(node1, node2):
+    if node1[0] > node2[0]:
+        return 'U'
+    elif node1[0] < node2[0]:
+        return 'D'
+    elif node1[1] < node2[1]:
+        return 'R'
+    else:
+        return 'L'
 
 
 def _shift_coordinate(i: int, j: int, move: str) -> tuple[int, int]:
@@ -60,8 +108,9 @@ def _shift_coordinate(i: int, j: int, move: str) -> tuple[int, int]:
 
 
 if __name__ == "__main__":
-    maze = Maze.from_file("practicum_2/homework/maze_2.txt")
+    maze = Maze.from_file("maze_2.txt")
     t_start = perf_counter()
     solve(maze)
+    maze.draw_graph()
     t_end = perf_counter()
     print(f"Elapsed time: {t_end - t_start} sec")
--- practicum_2/homework/dfs_graph.py
+++ practicum_2/homework/dfs_graph.py
@@ -3,6 +3,9 @@ from typing import Any
 
 import networkx as nx
 
+import sys, os
+sys.path.append(os.getcwd())
+print(sys.path)
 from src.plotting import plot_graph
 
 
@@ -12,23 +15,35 @@ def visit(node: Any):
 
 def dfs_iterative(G: nx.Graph, node: Any):
     visited = {n: False for n in G}
-
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
-
+    stack = {node}
+    while len(stack) > 0:
+        nd = stack.pop()
+        visit(nd)
+        visited[nd] = True
+        for x in G.neighbors(nd):
+            if not visited[x]: stack.add(x)
 
 def topological_sort(G: nx.DiGraph, node: Any):
     visited = {n: False for n in G}
-
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    stack = [node]
+    sorted_stack = []
+    while len(stack) > 0:
+        unvisited = [n for n in G.neighbors(stack[-1]) if not visited[n] and not n in stack]
+        if len(unvisited) == 0:
+            n = stack.pop()
+            visited[n] = True
+            sorted_stack.append(n)
+        else:
+            stack += unvisited
+    m = dict()
+    for i in range(len(sorted_stack)):
+        m.update({sorted_stack.pop(): str(i)})
+    return nx.relabel.relabel_nodes(G, m)
 
 
 if __name__ == "__main__":
     # Load and plot the graph
-    G = nx.read_edgelist("practicum_2/homework/graph_2.edgelist", create_using=nx.Graph)
+    G = nx.read_edgelist("graph_2.edgelist", create_using=nx.Graph)
     # plot_graph(G)
 
     print("Iterative DFS")
@@ -37,9 +52,14 @@ if __name__ == "__main__":
     print()
 
     G = nx.read_edgelist(
-        "practicum_2/homework/graph_2.edgelist", create_using=nx.DiGraph
+        "graph_2.edgelist", create_using=nx.DiGraph
     )
-    plot_graph(G)
+    print("-" * 32)
     print("Topological sort")
     print("-" * 32)
-    topological_sort(G, node="0")
+    sorted_nodes = topological_sort(G, node='0')
+    for node in sorted_nodes:
+        print(f"Wow, it is {node} right here!")
+
+    plot_graph(G)
+    plot_graph(topological_sort(G, node="0"))
