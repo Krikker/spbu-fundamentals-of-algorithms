280d116d9dfd94f0f90fe9321eb7ac520b853f94
Krikker <andrey.leonov.2004@mail.ru>
2023-04-05 19:15:05 +0300

    practicum 3 complete

--- practicum_3/homework/mst.py
+++ practicum_3/homework/mst.py
@@ -2,7 +2,10 @@ from typing import Any
 
 import matplotlib.pyplot as plt
 import networkx as nx
+import numpy as np
 
+import sys, os
+sys.path.append(os.getcwd())
 from src.plotting import plot_graph
 
 
@@ -11,15 +14,32 @@ def prim_mst(G: nx.Graph, start_node="0") -> set[tuple[Any, Any]]:
     rest_set = set(G.nodes())  # set of nodes not yet included into MST
     mst_edges = set()  # set of edges constituting MST
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    mst_set.add(start_node)
+    rest_set.remove(start_node)
+    considered_edges = {(start_node, x) for x in G.neighbors(start_node)}
+    while len(rest_set) > 0:
+        min_edge = {
+            "edge": (None, None),
+            "weight": np.inf
+        }
+        for e in considered_edges:
+            if G[e[0]][e[1]]["weight"] < min_edge["weight"]:
+                min_edge["edge"] = e
+                min_edge["weight"] = G[e[0]][e[1]]["weight"]
+        mst_edges.add(min_edge["edge"])
+        new_node = min_edge["edge"][1]
+        mst_set.add(new_node)
+        rest_set.remove(new_node)
+        for n in G.neighbors(new_node):
+            if n in mst_set:
+                considered_edges.remove((n, new_node))
+            else:
+                considered_edges.add((new_node, n))
 
     return mst_edges
 
-
 if __name__ == "__main__":
-    G = nx.read_edgelist("practicum_3/homework/graph_1.edgelist", create_using=nx.Graph)
+    G = nx.read_edgelist("graph_1.edgelist", create_using=nx.Graph)
     plot_graph(G)
     mst_edges = prim_mst(G, start_node="0")
     plot_graph(G, highlighted_edges=list(mst_edges))
--- practicum_3/homework/sp.py
+++ practicum_3/homework/sp.py
@@ -1,22 +1,41 @@
 from typing import Any
 
 import networkx as nx
+import numpy as np
+from queue import PriorityQueue
 
+import sys, os
+sys.path.append(os.getcwd())
 from src.plotting import plot_graph
 
 
 def dijkstra_sp(G: nx.Graph, source_node="0") -> dict[Any, list[Any]]:
-    shortest_paths = {}  # key = destination node, value = list of intermediate nodes
+    shortest_paths = {source_node : [source_node]}  # key = destination node, value = list of intermediate nodes
+    dists = {n: np.inf for n in G}
+    dists[source_node] = 0
+    visited = set()
+    pq = PriorityQueue()
+    pq.put((0, source_node))
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    while not pq.empty():
+        mon_dist, node = pq.get()
+        if node in visited:
+            continue
+        visited.add(node)
+        for n in G.neighbors(node):
+            if n in visited:
+                continue
+            dist = G[n][node]["weight"] + mon_dist
+            if dist < dists[n]:
+                dists[n] = dist
+                shortest_paths[n] = shortest_paths[node] + [n]
+                pq.put((dist, n))
 
     return shortest_paths
 
 
 if __name__ == "__main__":
-    G = nx.read_edgelist("practicum_3/homework/graph_1.edgelist", create_using=nx.Graph)
+    G = nx.read_edgelist("graph_1.edgelist", create_using=nx.Graph)
     plot_graph(G)
     shortest_paths = dijkstra_sp(G, source_node="0")
     test_node = "5"
