dd76e57b0353b83488899078f04f7403e1252109
Krikker <andrey.leonov.2004@mail.ru>
2023-04-13 23:54:10 +0300

    practicum 5 complete

--- practicum_5/homework/convex_bucket.py
+++ practicum_5/homework/convex_bucket.py
@@ -8,18 +8,38 @@ from src.plotting import plot_points
 
 def convex_bucket(points: NDArray) -> NDArray:
     """Complexity: O(n log n)"""
-    clockwise_sorted_ch = []
+    def ccw(p1, p2, p3):
+        return (p3[1] - p1[1]) * (p2[0] - p1[0]) > (p2[1] - p1[1]) * (p3[0] - p1[0])
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    def dist(p1, p2):
+        return np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
 
-    return np.array(clockwise_sorted_ch)
+    sorted_index = np.argsort(points[:, 0])
+    sorted_points = points[sorted_index]
+    p0 = sorted_points[0]
+    stack = [p0, sorted_points[1]]
+
+    for i in range(2, len(sorted_points)):
+        while len(stack) >= 2 and not ccw(stack[-2], stack[-1], sorted_points[i]):
+            stack.pop()
+        stack.append(sorted_points[i])
+    stack.pop()
+
+    p_max_x = sorted_points[-1]
+    if np.array_equal(p0, stack[-1]) and np.array_equal(p_max_x, stack[0]):
+        stack_x = np.array(stack)
+        p_min_x = stack_x[:, 0].min()
+        p_max_x = stack_x[:, 0].max()
+        stack = [p for p in stack if p[0] != p_min_x and p[0] != p_max_x]
+
+    stack.insert(0, p0)
+    stack.append(p_max_x)
+    return np.array(stack + stack[::-1])
 
 
 if __name__ == "__main__":
     for i in range(1, 11):
-        txtpath = f"practicum_5/homework/points_{i}.txt"
+        txtpath = f"points_{i}.txt"
         points = np.loadtxt(txtpath)
         print(f"Processing {txtpath}")
         print("-" * 32)
